8325bcfdbfadb8494dc3419f40e2dda33bc41c6b
NinaVersin <ninaversin@hotmail.com>
2015-05-23 11:54:58 +0200

    TODO getTile and SearchObj

--- src/jumpingalien/program/expression/GetTile.java
+++ src/jumpingalien/program/expression/GetTile.java
@@ -29,7 +29,6 @@ public class GetTile extends Expression {
 	public Tile evaluate(Map<String,Type> globals) {
 		double x = ((Constant<Double>)getLeftExpression()).evaluate(globals);
 		double y = ((Constant<Double>)getRightExpression()).evaluate(globals);
-		System.out.println(globals.get("this"));
 		World world = ((ObjectType)globals.get("this")).getValue().getWorld();
 		int feature = world.getFeatureAt((int) Math.round(x), (int) Math.round(y));
 		return new Tile(x,y,world,feature);
